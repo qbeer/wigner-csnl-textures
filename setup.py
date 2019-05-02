4a6e12e0dafd17c7c3aba65eb5ebf53d248c65f6
Alex Olar <olaralex666@gmail.com>
2019-05-02 16:11:02 +0200

    changed only decay in vae abstract class and lowered beta

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='csnl-vae-olaralex',
-    version='0.6dev',
+    version='0.8dev',
     author="Alex Olar",
     author_email="olaralex@caesar.elte.hu",
     packages=find_packages(),
