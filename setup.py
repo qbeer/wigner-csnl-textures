d0f22a12f61f0a999dac86aee4cc798b5329c0a1
Alex Olar <olaralex666@gmail.com>
2019-05-28 17:04:06 +0200

    changes v

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='csnl-vae-olaralex',
-    version='1.49dev',
+    version='1.50dev',
     author="Alex Olar",
     author_email="olaralex@caesar.elte.hu",
     packages=find_packages(),
