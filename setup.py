8c756fc378a3a4d7837f3de2b4999567a55685e1
Alex Olar <olaralex666@gmail.com>
2019-06-22 11:37:10 +0200

    kaggle sucks

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='csnl-vae-olaralex',
-    version='1.79dev',
+    version='1.80dev',
     author="Alex Olar",
     author_email="olaralex@caesar.elte.hu",
     packages=find_packages(),
