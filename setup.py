504dcff74416e708c16d27c3922bf373734f1ad1
Alex Olar <olaralex666@gmail.com>
2019-06-22 11:00:04 +0200

    necessery version requirements for kaggle run

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='csnl-vae-olaralex',
-    version='1.78dev',
+    version='1.79dev',
     author="Alex Olar",
     author_email="olaralex@caesar.elte.hu",
     packages=find_packages(),
