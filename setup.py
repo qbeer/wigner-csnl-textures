8aa66799902bc423eab63705b5546d2b8309138f
Alex Olar <olaralex666@gmail.com>
2019-07-04 12:38:43 +0200

    version incr, lvae picture update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='csnl-vae-olaralex',
-    version='1.87dev',
+    version='1.88dev',
     author="Alex Olar",
     author_email="olaralex@caesar.elte.hu",
     packages=find_packages(),
