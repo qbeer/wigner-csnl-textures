c92f92b1c983271ac3ac166b4de2e8a7804a59c9
Alex Olar <qbeer666@q.krft>
2019-08-27 09:37:23 +0200

    keras version is really important

--- csnl/ladder_variational_autoencoders/conv_lvae.py
+++ csnl/ladder_variational_autoencoders/conv_lvae.py
@@ -1,5 +1,5 @@
 from .ladder_vae import LadderVAE
-from keras.layers import Input, Conv2D, ReLU, Dense, MaxPooling2D, Flatten, Reshape, Deconv2D
+from keras.layers import Input, Conv2D, ReLU, Dense, MaxPooling2D, Flatten, Reshape
 from keras.models import Model
 import numpy as np
 
@@ -21,7 +21,7 @@ class ConvLadderVAE(LadderVAE):
         return encoder
 
     def encoder2(self):
-        input_img = Input(shape=(self.latent_dim1,))
+        input_img = Input(shape=(self.latent_dim1, ))
         x = Dense(256)(input_img)
         x = Reshape(target_shape=(16, 16, 1))(x)
         x = Conv2D(128, (2, 2), padding='same')(x)
@@ -35,7 +35,7 @@ class ConvLadderVAE(LadderVAE):
         return encoder
 
     def decoder2(self):
-        latent2 = Input(shape=(self.latent_dim2,))
+        latent2 = Input(shape=(self.latent_dim2, ))
         x = Dense(256)(latent2)
         x = Reshape(target_shape=(16, 16, 1))(x)
         x = Conv2D(128, (2, 2), padding='same')(x)
@@ -50,7 +50,7 @@ class ConvLadderVAE(LadderVAE):
         return decoder
 
     def decoder1(self):
-        latent1 = Input(shape=(self.latent_dim1,))
+        latent1 = Input(shape=(self.latent_dim1, ))
         x = Dense(1024)(latent1)
         x = Reshape(target_shape=(32, 32, 1))(x)
         x = Conv2D(256, (2, 2), padding='same')(x)
--- test.py
+++ test.py
@@ -1,8 +1,43 @@
 import os
-from csnl import DataGeneratorWithLabels
+from csnl import DataGeneratorWithLabels, DataGenerator, \
+    DenseLadderVAE, VAEPlotter, ModelTrainer
 
-data_gen = DataGeneratorWithLabels(image_shape=(28, 28, 1),
-                                   batch_size=70,
-                                   file_path=os.getcwd() +
-                                   '/csnl/data/scramtex_700_28px.pkl',
-                                   whiten=True)
+import tensorflow as tf
+import tensorflow_probability as tfp
+import keras as K
+
+print(tf.__version__, tfp.__version__, K.__version__)
+
+data_gen_labels = DataGeneratorWithLabels(image_shape=(28, 28, 1),
+                                          batch_size=70,
+                                          file_path=os.getcwd() +
+                                          '/csnl/data/scramtex_700_28px.pkl',
+                                          whiten=False)
+
+data_gen = DataGenerator(image_shape=(28, 28, 1),
+                         batch_size=70,
+                         file_path=os.getcwd() +
+                         '/csnl/data/scramtex_700_28px.pkl',
+                         whiten=False)
+
+LATENT_DIM1 = 16 * 4
+LATENT_DIM2 = 16
+
+vae = DenseLadderVAE(input_shape=(70, 28 * 28),
+                     latent_dim1=LATENT_DIM1,
+                     latent_dim2=LATENT_DIM2)
+
+trainer = ModelTrainer(vae,
+                       data_gen,
+                       loss_fn="normal",
+                       lr=5e-4,
+                       decay=1e-4,
+                       beta=1)
+
+trainer.fit(10, 2000, contrast=True, warm_up=True, make_gif=True)
+
+plotter = VAEPlotter(trainer, data_gen, data_gen_labels, grid_size=7)
+plotter.plot_contrast_correlations()
+plotter.plot_label_correlations()
+plotter.grid_plot()
+plotter.generate_samples()
