96a8d89af34d77949732e20568e2075a0bca5fca
Alex Olar <qbeer666@q.krft>
2019-08-29 14:50:10 +0200

    moved 4 to 5

---
+++ test_runs/5/test.py
@@ -0,0 +1,37 @@
+import os
+from csnl import DataGeneratorWithLabels, DataGenerator, \
+    SmallDenseLadderVAE, VAEPlotter, ModelTrainer
+
+data_gen_labels = DataGeneratorWithLabels(image_shape=(28, 28, 1),
+                                          batch_size=100,
+                                          file_path=os.getcwd() +
+                                          '/csnl/data/textures_42000_28px.pkl',
+                                          whiten=False, contrast_normalize=True)
+
+data_gen = DataGenerator(image_shape=(28, 28, 1),
+                         batch_size=100,
+                         file_path=os.getcwd() +
+                         '/csnl/data/textures_42000_28px.pkl',
+                         whiten=False, contrast_normalize=True)
+
+LATENT_DIM1 = 16 * 8
+LATENT_DIM2 = 16 * 2
+
+vae = SmallDenseLadderVAE(input_shape=(100, 28 * 28),
+                     latent_dim1=LATENT_DIM1,
+                     latent_dim2=LATENT_DIM2)
+
+trainer = ModelTrainer(vae,
+                       data_gen,
+                       loss_fn="normal",
+                       lr=5e-5,
+                       decay=1e-5,
+                       beta=1)
+
+trainer.fit(250, 1000, contrast=True, warm_up=True, make_gif=True)
+
+plotter = VAEPlotter(trainer, data_gen, data_gen_labels, grid_size=8)
+plotter.plot_contrast_correlations(latent_dim2=LATENT_DIM1)
+plotter.plot_label_correlations()
+plotter.grid_plot()
+plotter.generate_samples()
