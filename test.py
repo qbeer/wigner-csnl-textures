9ebebf777ab29946bea01423bb332c0a76c7fdb1
Alex Olar <qbeer666@q.krft>
2019-09-13 11:45:38 +0200

    cifar 10 run added, nice results

--- test.py
+++ test.py
@@ -12,19 +12,14 @@ from shutil import copyfile
 from csnl import DataGeneratorWithLabels, DataGenerator, \
     DenseLadderVAE, VAEPlotter, ModelTrainer
 
-data_gen = DataGenerator(image_shape=(28, 28, 1),
+data_gen = DataGenerator(image_shape=(28, 28, 3),
                          batch_size=100,
-                         file_path=os.getcwd() + "/csnl/data/mnist.pkl")
-
-label_data_gen = DataGeneratorWithLabels(image_shape=(28, 28, 1),
-                                         batch_size=100,
-                                         file_path=os.getcwd() +
-                                         "/csnl/data/mnist.pkl")
+                         file_path=os.getcwd() + "/csnl/data/cifar10.pkl")
 
 LATENT_DIM2 = 16
 LATENT_DIM1 = 16 * 4
 
-vae = DenseLadderVAE(input_shape=(100, 28 * 28),
+vae = DenseLadderVAE(input_shape=(100, 28 * 28 * 3),
                      latent_dim1=LATENT_DIM1,
                      latent_dim2=LATENT_DIM2)
 
@@ -35,13 +30,11 @@ trainer = ModelTrainer(vae,
                        decay=1e-5,
                        beta=100)
 
-trainer.fit(1000, 1000, warm_up=True)
+trainer.fit(2000, 1000, warm_up=True)
 
-plotter = VAEPlotter(trainer, data_gen, label_data_gen, grid_size=10)
+plotter = VAEPlotter(trainer, data_gen, data_gen, grid_size=10)
 
 plotter.grid_plot()
 plotter.generate_samples()
-plotter.plot_contrast_correlations()
-plotter.plot_label_correlations()
 
 copyfile(os.getcwd() + "/test.py", os.getcwd() + "/results/test.py")
