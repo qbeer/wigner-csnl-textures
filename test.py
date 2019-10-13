9487949a90d55ccafe02c8ebd960e9a4b3898e1b
Alex Olar <qbeer666@q.krft>
2019-10-13 15:23:25 +0200

    new run

--- test.py
+++ test.py
@@ -7,19 +7,21 @@ from shutil import copyfile
 data_gen_labels = DataGeneratorWithLabels(image_shape=(28, 28, 1),
                                           batch_size=100,
                                           file_path=os.getcwd() +
-                                          '/csnl/data/textures_42000_28px.pkl')
+                                          '/csnl/data/textures_42000_28px.pkl',
+                                          contrast_normalize=True)
 
 data_gen = DataGenerator(image_shape=(28, 28, 1),
                          batch_size=100,
                          file_path=os.getcwd() +
-                         '/csnl/data/textures_42000_28px.pkl')
+                         '/csnl/data/textures_42000_28px.pkl',
+                         contrast_normalize=True)
 
 LATENT_DIM1 = 16 * 4
 LATENT_DIM2 = 16
 
-vae = DenseLadderVAE(input_shape=(100, 28*28*1),
-                    latent_dim1=LATENT_DIM1,
-                    latent_dim2=LATENT_DIM2)
+vae = DenseLadderVAE(input_shape=(100, 28 * 28 * 1),
+                     latent_dim1=LATENT_DIM1,
+                     latent_dim2=LATENT_DIM2)
 
 trainer = ModelTrainer(vae,
                        data_gen,
